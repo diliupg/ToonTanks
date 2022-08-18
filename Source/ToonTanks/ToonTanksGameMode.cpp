bfaaa3c1fc1407f46e916c22e36e7135b3705aac
diliupg <unitygamecodersl@gmail.com>
2022-08-18 21:53:33 +0530

    L144 winning and Losing done.

--- Source/ToonTanks/ToonTanksGameMode.cpp
+++ Source/ToonTanks/ToonTanksGameMode.cpp
@@ -16,11 +16,17 @@ void AToonTanksGameMode::ActorDied( AActor* DeadActor )
 		{
 			ToonTanksPlayerController->SetPlayerEnabledState( false );
 		}
+		GameOver( false );
 		
 	}
 	else if ( ATower* DestroyedTower = Cast<ATower>( DeadActor ) )
 	{
 		DestroyedTower->HandleDestruction( );
+		TargetTowers--;
+		if ( TargetTowers == 0 )
+		{
+			GameOver( true );
+		}
 	}
 }
 
@@ -34,6 +40,8 @@ void AToonTanksGameMode::BeginPlay( )
 
 void AToonTanksGameMode::HandleGameStart( )
 {
+	TargetTowers = GetTargetTowerCount( );
+
 	Tank = Cast<ATank>( UGameplayStatics::GetPlayerPawn( this, 0 ) );
 	ToonTanksPlayerController = Cast<AToonTanksPlayerController>( UGameplayStatics::GetPlayerController( this, 0 ) );
 
@@ -59,4 +67,11 @@ void AToonTanksGameMode::HandleGameStart( )
 			false
 		);
 	}
+}
+
+int32 AToonTanksGameMode::GetTargetTowerCount( )
+{
+	TArray<AActor*> Towers;
+	UGameplayStatics::GetAllActorsOfClass( this, ATower::StaticClass( ), Towers );
+	return Towers.Num( );
 }
